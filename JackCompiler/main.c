9e7b70aabbaa332e3cb65f35355011c0e05eb56a
Stan Stevens <sstevens404@gmail.com>
2017-07-28 12:37:44 -0400

    Initial Commit

---
+++ JackCompiler/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  JackCompiler
+//
+//  Created by Stanford Stevens on 7/28/17.
+//  Copyright © 2017 Stanford Stevens. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
