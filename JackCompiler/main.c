f3bcd9d4324579fc678e77345168adac3024373e
Stan Stevens <sstevens404@gmail.com>
2017-07-28 13:02:57 -0400

    Initial commit.
    Correctly identify '.jack' files and directories, creating output file for each input file.

--- JackCompiler/main.c
+++ JackCompiler/main.c
@@ -7,9 +7,100 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+#include <ctype.h>
+
+int is_jack_file(const char *file) {
+    const char *dot = strrchr(file, '.');
+    if(!dot || dot == file) { return 0; }
+    
+    const char *extension = dot + 1;
+    return (strcmp(extension, "jack") == 0) ? 1 : 0;
+}
+
+char* trim_whitespace(char *string) {
+    while(isspace((unsigned char)*string)) {
+        string++;
+    }
+    
+    if(*string == 0) { return string; }
+    
+    char *end = string + strlen(string) - 1;
+    while(end > string && isspace((unsigned char)*end)) {
+        end--;
+    }
+    
+    *(end+1) = 0;
+    
+    return string;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    char *filepath = malloc(200);
+    printf("Enter filepath bitch> ");
+    scanf("%s", filepath);
+    
+    filepath = trim_whitespace(filepath);
+    
+    struct stat path_stat;
+    stat(filepath, &path_stat);
+    int number_of_files = 0;
+    char **files = malloc(sizeof(char *));
+    
+    if (S_ISDIR(path_stat.st_mode)) {
+        DIR *directory;
+        struct dirent *entry;
+        
+        directory = opendir(filepath);
+        if (directory != NULL) {
+            while ((entry = readdir(directory))) {
+                if (!is_jack_file(entry->d_name)) { continue; }
+                
+                number_of_files++;
+                files = realloc(files, number_of_files * sizeof(char *));
+                
+                size_t new_index = number_of_files - 1;
+                char *entry_path = malloc(strlen(filepath) + strlen(entry->d_name) + 1 + 1);
+                strcpy(entry_path, filepath);
+                strcat(entry_path, "/");
+                strcat(entry_path, entry->d_name);
+                size_t entry_length = strlen(entry_path) + 1;
+                files[new_index] = malloc(entry_length);
+                strcpy(files[new_index], entry_path);
+            }
+            
+            closedir(directory);
+        }
+    } else if (S_ISREG(path_stat.st_mode) && is_jack_file(filepath)) {
+        number_of_files = 1;
+        files[0] = malloc(strlen(filepath) + 1);
+        strcpy(files[0], filepath);
+    }
+    
+    if (number_of_files == 0) {
+        printf("No virtual machine files found\n");
+        return 1;
+    }
+
+    for (int i = 0; i < number_of_files; i++) {
+        char *filepath = files[i];
+        
+        //initialize output file
+        char outputPath[strlen(filepath) + 1];
+        strcpy(outputPath, filepath);
+        char *loc = strrchr(outputPath, '.') + 1;
+        *(loc) = 0;
+        strcat(outputPath, "vm");
+        FILE *outputFile = fopen(outputPath, "w");
+        
+        //TODO: add tokenizer and parser for each file
+        
+        fclose(outputFile);
+    }
+    
     return 0;
 }
